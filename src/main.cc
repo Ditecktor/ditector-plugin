bffa60f4d93806ccaa3a241565887aad661102bb
IkerGalardi <contacto.ikergalardi@gmail.com>
2021-02-07 17:26:21 +0100

    Fresh start of project

---
+++ src/main.cc
@@ -0,0 +1,33 @@
+/*
+ * This project is provided under the GNU GPL v2 license, more information can 
+ * found on https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * Authors:
+ *    - Iker Galardi
+ */
+
+#include <obs/obs-module.h>
+
+#include "plugin_info.hh"
+
+OBS_DECLARE_MODULE()
+OBS_MODULE_USE_DEFAULT_LOCALE(PLUGIN_NAME, "en-US")
+
+bool obs_module_load()
+{
+    blog(LOG_INFO, "plugin loaded successfully (version %s)", PLUGIN_VERSION);
+    return true;
+}
+
+void obs_module_unload()
+{
+    blog(LOG_INFO, "plugin unloaded");
+}
