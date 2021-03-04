84a8b8f791e5f5004151afb0e79bbf679d9fad9a
IkerGalardi <contacto.ikergalardi@gmail.com>
2021-03-04 19:44:47 +0100

    Added data files to installer

--- installer/linux.sh
+++ installer/linux.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+# Copy the binary into the obs folder
 cp build/obs-plugin.so /lib/obs-plugins/
+
+# Create the configuration folder if necessary 
+mkdir -p /usr/share/obs/obs-plugins/ditector
+cp -r data/* /usr/share/obs/obs-plugins/ditector
